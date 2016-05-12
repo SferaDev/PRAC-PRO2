df590a9b0eccab4b1a5d4a57c582c3a3d4de5900
Alexis Rico <me@sferadev.com>
2016-05-12 11:06:27 +0200

    Test: Update public script

--- jp/jp_public.sh
+++ jp/jp_public.sh
@@ -1,2 +1,2 @@
-time ./program.exe < jp_public_entrada.txt > out.o
-diff -bB jp_public_sortida.txt out.o > diff.o
+time ./program.exe < "jp/jp_public_entrada.txt" > out.o
+diff -bB "jp/jp_public_sortida.txt" out.o > diff.o
