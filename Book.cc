49b1ccbcff051e6c4e6a17f97232e33f988e1a70
Alexis Rico <me@sferadev.com>
2016-05-14 22:48:00 +0200

    Book: Better null check approach

--- Book.cc
+++ Book.cc
@@ -83,7 +83,7 @@ void Book::readBookContent() {
             formatString(line);
             trimString(content);
             // TODO: Empty lines should be added or we shouldn't add the text?
-            if (line.substr(0, line.length() - 1) != "") bookContent.push_back(line);
+            if (!line.substr(0, line.length() - 1).empty()) bookContent.push_back(line);
             string word;
             istringstream iss(line);
             while (iss >> word) {
