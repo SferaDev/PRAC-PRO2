3db1f97e15e34a27e3046a917e2aa38f43a2ad6b
Alexis Rico <me@sferadev.com>
2016-05-19 22:30:28 +0200

    Test: Add privat1 and privat3 executables

---
+++ test.sh
@@ -0,0 +1,7 @@
+make
+./program.exe < "jp_test.txt" > out0.o
+./privat1.sh < "jp_test.txt" > out1.o
+./privat3.sh < "jp_test.txt" > out3.o
+
+diff out0.o out1.o > diff1.o
+diff out0.o out3.o > diff3.o
