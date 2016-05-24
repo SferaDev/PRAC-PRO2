02cf9af2682c82c7bf5d2c41dba926827aa73c07
Alexis Rico <me@sferadev.com>
2016-05-24 18:43:03 +0200

    Doxygen: Update docs

--- DOC/html/search/all_c.js
+++ DOC/html/search/all_c.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['readbook',['readBook',['../class_library.html#a2e296d2dc8e0292f0ea6d8d3511f7ec5',1,'Library']]],
   ['readbookcontent',['readBookContent',['../class_book.html#a3e62d70f19bf6fa8ebef5556882b3ed7',1,'Book']]],
+  ['removedelimiter',['removeDelimiter',['../namespaceutils.html#adf961974462ef6e8d7ccc32311397d75',1,'utils']]],
   ['replacewords',['replaceWords',['../class_book.html#aaf182e24b86624b6ff54fba2581094a4',1,'Book']]],
   ['replacewordsonbook',['replaceWordsOnBook',['../class_library.html#abf34dcdd5eb2a7c4127e8a50db814750',1,'Library']]]
 ];
--- DOC/html/search/functions_2.js
+++ DOC/html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['contains',['contains',['../namespaceutils.html#a69c832543a093a8099189e4755695a62',1,'utils']]]
+  ['contains',['contains',['../namespaceutils.html#a69c832543a093a8099189e4755695a62',1,'utils']]],
+  ['containsconsecutive',['containsConsecutive',['../namespaceutils.html#af9af9e01b679f955003203f29ddf130b',1,'utils']]]
 ];
