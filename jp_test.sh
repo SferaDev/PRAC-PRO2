c1e6f3d780c19302a864257bad7434b1b7a704dd
Jordi Romero <jordi.romero.morcillo@est.fib.upc.edu>
2016-05-25 10:07:55 +0200

    Test: Update script

--- jp_test.sh
+++ jp_test.sh
@@ -1,5 +1,3 @@
 make
-./program.exe < "jp_test.txt" > out.mine.o
-./pau.sh < "jp_test.txt" > out.pau.o
-
-diff out.mine.o out.pau.o > diff2.o
+time ./program.exe < "jp_test.txt" > out.mine.o
+time ./pau.sh < "jp_test.txt" > out.pau.o
