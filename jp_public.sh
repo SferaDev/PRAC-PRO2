527c95b2660abb1a1eefc7e10489e1250e068e2b
Alexis Rico <me@sferadev.com>
2016-05-26 09:13:01 +0200

    Global: We are 4/4 so rm -rf *.*

--- jp_public.sh
+++
@@ -1,3 +0,0 @@
-make
-time ./program.exe < "jp_public_entrada.txt" > out.o
-diff "jp_public_sortida.txt" out.o > diff.o
--- jp_test.sh
+++
@@ -1,3 +0,0 @@
-make
-time ./program.exe < "jp_test.txt" > out.mine.o
-time ./pau.sh < "jp_test.txt" > out.pau.o
