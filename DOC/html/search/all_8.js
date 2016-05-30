397ad77a8e9cd1ba0988f0a7eb9fbb73d3cfaf1e
Alexis Rico <me@sferadev.com>
2016-05-30 11:22:53 +0200

    Doxygen: Update docs

--- DOC/html/search/all_8.js
+++ DOC/html/search/all_8.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['main',['main',['../program_8cc.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'program.cc']]],
-  ['malformedexpression',['malformedExpression',['../namespaceutils.html#a6c64225b3c1659211611f32e0776793b',1,'utils']]]
+  ['main',['main',['../program_8cc.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'program.cc']]]
 ];
